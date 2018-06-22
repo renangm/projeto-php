e210c2703ff2c926469d4cfcd85ffc094caf1bd8
Técnico em Informática 2018.1 <renangalmoura@gmail.com>
2018-06-21 21:07:16 -0300

    Inicio Projeto PHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+	$titulo = "Titulo da Pagina";
+	$tituloConteudo = "<h1>Titulo do Texto</h1>";
+	$conteudo = "<p>GIT - Versionamento</p>";
+?>
+<html>
+<head>
+	<title><?php echo $titulo; ?></title>
+	<meta charset="utf-8" />
+</head>
+<body>
+<?php 
+	echo $tituloContaudo;
+	echo $conteudo;
+?>
+</body>
+</html>
